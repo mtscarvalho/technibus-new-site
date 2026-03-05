5c6ae32f2b6df1aa87c6d2cdb8d12134e77933fb
Rafael de Araujo <rafaeldearaujop95@gmail.com>
2026-03-05 21:43:45 +0100

    chore(payload): update auto-generated admin import map for search plugin

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -14,6 +14,8 @@ import { ParagraphFeatureClient as ParagraphFeatureClient_e70f5e05f09f93e00b997e
 import { FixedToolbarFeatureClient as FixedToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
 import { UploadFeatureClient as UploadFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
 import { default as default_9e3e6f099ffedd9eb0502531c8bb6565 } from '@/fields/phoneNumber/component'
+import { LinkToDoc as LinkToDoc_aead06e4cbf6b2620c5c51c9ab283634 } from '@payloadcms/plugin-search/client'
+import { ReindexButton as ReindexButton_aead06e4cbf6b2620c5c51c9ab283634 } from '@payloadcms/plugin-search/client'
 import { default as default_3bad62110f863441cc632684bc7af343 } from '../../../components/Payload/DashboardIcon/index.tsx'
 import { default as default_1a2c36d784547fa77b725a86368390b8 } from '../../../components/Payload/DashboardLogo/index.tsx'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
@@ -35,6 +37,8 @@ export const importMap = {
   "@payloadcms/richtext-lexical/client#FixedToolbarFeatureClient": FixedToolbarFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
   "@payloadcms/richtext-lexical/client#UploadFeatureClient": UploadFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
   "@/fields/phoneNumber/component#default": default_9e3e6f099ffedd9eb0502531c8bb6565,
+  "@payloadcms/plugin-search/client#LinkToDoc": LinkToDoc_aead06e4cbf6b2620c5c51c9ab283634,
+  "@payloadcms/plugin-search/client#ReindexButton": ReindexButton_aead06e4cbf6b2620c5c51c9ab283634,
   "/components/Payload/DashboardIcon/index.tsx#default": default_3bad62110f863441cc632684bc7af343,
   "/components/Payload/DashboardLogo/index.tsx#default": default_1a2c36d784547fa77b725a86368390b8,
   "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
