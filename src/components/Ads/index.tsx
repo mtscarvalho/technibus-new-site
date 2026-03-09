12fbb6f484665b591f9435b9c1e96ccf08360ed8
Matheus Carvalho <henriquematheus.c@gmail.com>
2026-03-09 15:52:15 -0300

    Update Ads IDs

--- src/components/Ads/index.tsx
+++ src/components/Ads/index.tsx
@@ -6,40 +6,22 @@ type AdsProps = {
 export function Ads({ variant, className }: AdsProps) {
   switch (variant) {
     case "sidebarTopo":
-      return (
-        // /334660191/TB-barra_direita-300xX-topo
-        <div id="div-gpt-ad-1773058261868-0" className={className} style={{ minWidth: 300, minHeight: 250 }} />
-      );
+      return <div id="div-gpt-ad-1469728895277-2" className={className} style={{ minWidth: 300, minHeight: 250 }} />;
 
     case "sidebarMeio":
-      return (
-        // /334660191/TB-barra_direita-300xX-meio
-        <div id="div-gpt-ad-1773055055101-0" className={className} style={{ minWidth: 300, minHeight: 250 }} />
-      );
+      return <div id="div-gpt-ad-1469728895277-1" className={className} style={{ minWidth: 300, minHeight: 250 }} />;
 
     case "sidebarMeio2":
-      return (
-        // /334660191/TB-barra_direita-300xX-meio-2
-        <div id="div-gpt-ad-1773055055713-0" className={className} style={{ minWidth: 300, minHeight: 250 }} />
-      );
+      return <div id="div-gpt-ad-1469728895277-3" className={className} style={{ minWidth: 300, minHeight: 250 }} />;
 
     case "sidebarBase":
-      return (
-        // /334660191/TB-barra_direita-300xX-base
-        <div id="div-gpt-ad-1773055047019-0" className={className} style={{ minWidth: 300, minHeight: 250 }} />
-      );
+      return <div id="div-gpt-ad-1469728895277-0" className={className} style={{ minWidth: 300, minHeight: 250 }} />;
 
     case "principalMobile":
-      return (
-        // /334660191/TB-principal-320xX-mobile
-        <div id="div-gpt-ad-1773055056838-0" className={className} style={{ minWidth: 320, minHeight: 50 }} />
-      );
+      return <div id="div-gpt-ad-1469728895277-4" className={className} style={{ minWidth: 320, minHeight: 50 }} />;
 
     case "principalDesktop":
-      return (
-        // /334660191/TB-principal-970xX-desktop
-        <div id="div-gpt-ad-1773055057390-0" className={className} style={{ minWidth: 728, minHeight: 90 }} />
-      );
+      return <div id="div-gpt-ad-1469728895277-5" className={className} style={{ minWidth: 728, minHeight: 90 }} />;
 
     default:
       return <div />;
