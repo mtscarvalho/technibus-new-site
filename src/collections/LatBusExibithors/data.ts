9725bb81a548ba8b4b928038bf265cc58f1cb254
Rafael de Araujo <rafaeldearaujop95@gmail.com>
2026-03-09 06:00:03 +0100

    fixed data functions collection LatBusExibithors

--- src/collections/LatBusExibithors/data.ts
+++ src/collections/LatBusExibithors/data.ts
@@ -3,29 +3,14 @@ import { draftMode } from "next/headers";
 import config from "@payload-config";
 import { getPayload, PaginatedDocs } from "payload";
 
-import { Post } from "@/payload-types";
+import { LatBusExibithor } from "@/payload-types";
 
 const payload = await getPayload({ config });
 
-export const fetchPostBySlug = async (slug: string): Promise<Post> => {
+export const fetchPaginatedExibithors = async (page: number = 1): Promise<PaginatedDocs<LatBusExibithor>> => {
   const { isEnabled: draft } = await draftMode();
   const data = await payload.find({
-    collection: "posts",
-    depth: 1,
-    draft,
-    limit: 1,
-    where: {
-      and: [{ slug: { equals: slug } }, ...(draft ? [] : [{ _status: { equals: "published" } }])],
-    },
-  });
-
-  return data.docs[0];
-};
-
-export const fetchPaginatedPosts = async (page: number = 1): Promise<PaginatedDocs<Post>> => {
-  const { isEnabled: draft } = await draftMode();
-  const data = await payload.find({
-    collection: "posts",
+    collection: "latBusExibithors",
     depth: 1,
     draft,
     limit: 12,
@@ -38,10 +23,10 @@ export const fetchPaginatedPosts = async (page: number = 1): Promise<PaginatedDo
   return data;
 };
 
-export const fetchAllPosts = async (): Promise<Post[]> => {
+export const fetchAllExibithors = async (): Promise<LatBusExibithor[]> => {
   const { isEnabled: draft } = await draftMode();
   const data = await payload.find({
-    collection: "posts",
+    collection: "latBusExibithors",
     depth: 1,
     draft,
     limit: 0,
@@ -51,4 +36,17 @@ export const fetchAllPosts = async (): Promise<Post[]> => {
   });
 
   return data.docs;
+};
+
+export const fetchExibithorsSearch = async (where?: any): Promise<PaginatedDocs<LatBusExibithor>> => {
+  const data = await payload.find({
+    collection: "latBusExibithors",
+    depth: 1,
+    limit: 0,
+    where: {
+      and: [...(where ? [where] : [])],
+    },
+  });
+
+  return data;
 };
