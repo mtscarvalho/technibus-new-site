a2b54a7d66840a78a9587060567ac5b16fb47e99
Rafael de Araujo <rafaeldearaujop95@gmail.com>
2026-03-16 13:05:59 +0100

    remove access read do not used

--- src/collections/Posts/config.ts
+++ src/collections/Posts/config.ts
@@ -12,15 +12,6 @@ export const Posts: CollectionConfig = {
     singular: "Publicação",
     plural: "Publicações",
   },
-  access: {
-    read: ({ req }) => {
-      if (req.user && req.user?.collection === "users") return true;
-      return {
-        _status: { equals: "published" },
-        publishedDate: { less_than_equal: new Date().toISOString() },
-      };
-    },
-  },
   admin: {
     useAsTitle: "title",
     group: "Conteúdo",
