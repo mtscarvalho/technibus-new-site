7b876585ca70b7d4a000332e03383023f5427325
Rafael de Araujo <rafaeldearaujop95@gmail.com>
2026-03-17 04:51:26 +0100

    configure PayloadPluginCloudflarePurge

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,7 +1,9 @@
 import { searchPlugin } from "@/collections/Search/config";
+import { Category } from "@/payload-types";
 import { seoPlugin } from "@payloadcms/plugin-seo";
 import { s3Storage } from "@payloadcms/storage-s3";
 import { Plugin } from "payload";
+import { PayloadPluginCloudflarePurge } from "payload-plugin-cloudflare-purge";
 
 const plugins: Plugin[] = [
   seoPlugin({}),
@@ -20,6 +22,41 @@ const plugins: Plugin[] = [
     },
   }),
   searchPlugin,
+  // TODO: Testar posts agendados para ver se o cache esta sendo limpo normalmente
+  PayloadPluginCloudflarePurge({
+    enabled: process.env.NODE_ENV === "production",
+    zoneId: process.env.CLOUDFLARE_ZONE_ID!,
+    apiToken: process.env.CLOUDFLARE_API_TOKEN!,
+    baseUrl: process.env.SITE_URL!,
+    collections: ["posts"],
+    showButtonPurgeEverything: true,
+    purgeEverything: false,
+    urlBuilder: async ({ doc, req, collectionSlug, baseUrl }) => {
+      if (collectionSlug !== "posts") {
+        return [];
+      }
+
+      const categoryUrls: Promise<string>[] = [];
+
+      if (doc.category && Array.isArray(doc.category) && doc.category.length > 0) {
+        for (const id of doc.category as Category[]) {
+          categoryUrls.push(
+            (async () => {
+              const category = await req.payload.findByID({
+                id: Number(id),
+                collection: "categories",
+                draft: false,
+              });
+              return `${baseUrl}${category.relPermalink}`;
+            })(),
+          );
+        }
+      }
+
+      const resolvedCategoryUrls = await Promise.all(categoryUrls);
+      return [`${baseUrl}${doc.relPermalink}`, `${baseUrl}/`, ...resolvedCategoryUrls];
+    },
+  }),
 ];
 
 export default plugins;
